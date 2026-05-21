76db7ca13c7e2f463fa30350ae9c01521a8bec69
Penzen Lama <penzenlama@gmail.com>
2026-05-21 12:41:04 +0200

    Added logging files to the project

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.student_Performance_p1 import logger
+
+
+
+logger.info("Welcome to custom logging in Python!")
---
+++ setup.py
@@ -0,0 +1,1 @@
+# this is to provide the setup for the package, so that it can be installed and used in other projects
